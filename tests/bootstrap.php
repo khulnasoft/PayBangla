1c11aaf31d44fa62b14c96620a455c9fad3d1544
xeondesk <xeondesk@gmail.com>
2026-04-15 05:04:38 +0600

    🔧 fix: update test bootstrap to find wp-phpunit in vendor

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,11 +8,11 @@
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 
 if ( ! $_tests_dir ) {
-	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+	$_tests_dir = dirname( __DIR__ ) . '/vendor/wp-phpunit/wp-phpunit';
 }
 
 if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
-	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL;
+	echo "Could not find $_tests_dir/includes/functions.php, have you run composer install ?" . PHP_EOL;
 	exit( 1 );
 }
 
